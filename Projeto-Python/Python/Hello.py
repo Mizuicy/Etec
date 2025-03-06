8858911c96b0f4cf391e00e696e81012b551a4af
Gustavo Oliveira <gustavosilva90p@gmail.com>
2025-03-05 23:04:04 -0300

    Python o basico de variaveis

---
+++ Projeto-Python/Python/Hello.py
@@ -0,0 +1,14 @@
+nome = "Gustavo"
+sobrenome = "Oliveira"
+idade = 24
+ano_de_nascimento = 2025 - idade
+maior_de_idade = idade >= 18 
+altura_metros = 1.82
+
+
+print('Nome', nome)
+print('Sobrenome', sobrenome)
+print('Idade', idade)
+print('Ano de Nascimento', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em Metros', altura_metros)
