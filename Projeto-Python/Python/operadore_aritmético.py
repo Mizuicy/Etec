f6dab13e5d3ef25c0b9d98b576d1256135fdd5d0
Gustavo Oliveira <gustavosilva90p@gmail.com>
2025-03-07 00:16:27 -0300

    Operadores Aritméticos em python

---
+++ Projeto-Python/Python/operadore_aritmético.py
@@ -0,0 +1,21 @@
+adicao = 10 + 10 
+print('Adição', adicao)
+
+subtracao = 10 - 9
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 5
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 
+print('Divisão', divisao)
+
+divisao_inteiro = 10 // 2
+print('Divisão inteira', divisao_inteiro)
+
+exponenciacao = 3 ** 10 
+print('Exponenciação', exponenciacao)
+
+modulo = 3 % 2 #Retorna o resto da divisão
+print('Modulo', modulo)
+
