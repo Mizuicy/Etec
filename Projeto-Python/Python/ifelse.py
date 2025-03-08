452e4e916521f102c3850e0e3399ce8d4d8fb492
Gustavo Oliveira <gustavosilva90p@gmail.com>
2025-03-08 20:57:37 -0300

    Entendedo o funcionamento do IF/ELIF/ELSE PYTHON

---
+++ Projeto-Python/Python/ifelse.py
@@ -0,0 +1,14 @@
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Este é o codigo do if')
+elif condicao2:
+    print('Condição 2')
+elif condicao3:
+    print('Condição 3')
+else:
+    print('Condição 4')
