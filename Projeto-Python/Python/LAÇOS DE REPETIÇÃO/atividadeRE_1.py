b4cadbeec0e61bfafe91de6f61016a31fd4c8509
Camillo11 <camillyvblackswan7@gmail.com>
2025-04-28 22:27:10 -0300

    atualização_do_repositorio_no_meu_pc

---
+++ Projeto-Python/Python/LAÇOS DE REPETIÇÃO/atividadeRE_1.py
@@ -0,0 +1,13 @@
+import random
+contador = int()
+
+
+for numberInt in range(1, 9, 1):
+    # numberInt = int(input(f"Digite o {numberInt}° valor"))
+    numberInt = random.randint(0, 200)
+    print(numberInt)
+
+    if numberInt >= 10 and numberInt <= 150:
+        contador += 1
+
+print(f"Essa quantidade {contador} está entre 10 e 150")
