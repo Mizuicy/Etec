3580a1ed449f99805f8c3bbf54c79b61063c8207
Gustavo Oliveira <gustavosilva90p@gmail.com>
2025-05-12 16:14:21 -0300

    7_incompleta_e_8_Concluida

---
+++ Projeto-Python/Python/LAÇOS DE REPETIÇÃO/atividadeRE_7.py
@@ -0,0 +1,11 @@
+i = 0
+
+while i < 41:
+    precoCusto = float(input("Digite o preço Custo"))
+    precoVenda = float(input("Digite o preço Venda"))
+    lucro = precoVenda - precoCusto
+    prejuizo = precoCusto - precoVenda
+    empate = float
+    
+    
+    
---
+++ Projeto-Python/Python/LAÇOS DE REPETIÇÃO/atividadeRE_8.py
@@ -0,0 +1,26 @@
+i = 0
+while i < 10:
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    sexo = input("Digite seu sexo (M)Masculino (F)Feminino: ")
+    salario = 0,0
+    
+    if sexo.upper() == "M" and idade >= 30:
+        salario = 100
+    elif sexo.upper() == "M" and idade < 30:
+        salario = 50
+    elif sexo.upper() == "F" and idade >= 30:
+        salario = 200
+    elif sexo.upper() == "F" and idade < 30:
+        salario = 80
+    else:
+        print("Sexo inválido")
+        continue
+    
+    print(f"Nome: {nome}")
+    print(f"Idade: {idade}")
+    print(f"Sexo: {sexo}")
+    print(f"Salario: R$:{salario}")
+    i += 1
+
+print("Fim do programa")
