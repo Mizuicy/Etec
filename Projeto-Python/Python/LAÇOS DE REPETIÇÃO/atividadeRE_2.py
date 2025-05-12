81b19acb3dc5fd837d0ceb5fb0456e87dfb20618
Gustavo Oliveira <gustavosilva90p@gmail.com>
2025-05-11 23:49:58 -0300

    concluido

--- Projeto-Python/Python/LAÇOS DE REPETIÇÃO/atividadeRE_2.py
+++ Projeto-Python/Python/LAÇOS DE REPETIÇÃO/atividadeRE_2.py
@@ -1,16 +1,26 @@
-for numberInt in range(0, 8, 1):
+#atualização do codigo
 
-    nome = input("Informe seu nome")
-    idade = int(input("Informe sua idade"))
-    saude = input("Informe sua saude está boa (S)-Sim se não (N)-Não ")
+TotalApto = 0
+TotalInapto = 0
 
-if idade >= 18:
-    if saude.upper() == "S":
+numberInt = int(input("Informe quantas pessoas você quer cadastrar: "))
+
+for i in range(numberInt):
+
+    nome = input("Informe seu nome =")
+    idade = int(input("Informe sua idade ="))
+    saude = input("Informe sua saude está boa (S)-Sim se não (N)-Não =")
+
+    if idade >= 18 and saude.upper() == "S":
         print(f"Senhor(a){nome}, idade {idade}, e esta bem de saude tem todos os requisitos para servir")
+        TotalApto += 1
     else:
-        print(f"Senhor(a){nome}, idade {idade},mas não está bem de saude")
-else:
-    print(f"Senhor(a){nome},mas não tem idade suficiente {idade}")
+        print(f"Senhor(a){nome},mas não tem idade suficiente {idade}")
+        TotalInapto += 1
+
+
+print(f"__Total de aptos: {TotalApto}")
+print(f"__Total de inapto: {TotalInapto}")
 
 
 
