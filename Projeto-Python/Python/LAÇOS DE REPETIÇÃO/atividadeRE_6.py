29b3af6ef902d264da790219d2004b41d0fcabaa
Gustavo Oliveira <gustavosilva90p@gmail.com>
2025-05-12 12:31:58 -0300

    atividade_6_concluida

---
+++ Projeto-Python/Python/LAÇOS DE REPETIÇÃO/atividadeRE_6.py
@@ -0,0 +1,28 @@
+carros_2000 = 0
+carros_2001 = 0
+
+
+while True:
+    valorCarro = float(input("Digite o valor do carro: "))
+    anoCarro = int(input("Digite o ano do carro: "))
+    valorPaga = float
+    if anoCarro <= 2000:
+        descontoCarro = valorCarro * 0.12
+        carros_2000 += 1
+    elif anoCarro > 2000:
+        descontoCarro = valorCarro * 0.07
+        carros_2001 += 1
+    else:
+        print("Ano inválido")
+        continue
+    
+    valorCarro -= descontoCarro
+    print(f"Desconto de: {descontoCarro}")
+    print(f"Valor do carro com desconto: {valorCarro}")
+    
+    resposta = input("Deseja continuar calculando (S/N)")
+    if resposta.upper() == "N":
+        print(f"Total de carros 2000: {carros_2000}")
+        print(f"Total de carros 2001: {carros_2001}")
+        print(f"Total de carros com desconto: {carros_2000 + carros_2001}")
+        break
